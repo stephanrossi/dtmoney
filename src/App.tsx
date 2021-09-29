8fd7b5b67e220fd0f78bee26e9ec5e6be0cc0bba
Stephan Rossi <stephanrossi@gmail.com>
2021-09-29 13:58:34 -0300

    29/09

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,58 @@
+import { useState } from "react"
+
 import { Header } from "./components/Header"
 import { Dashboard } from "./components/Dashboard"
 import { GlobalStyle } from "./styles/global"
 
+import Modal from "react-modal"
+
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+        style={{
+          overlay: {
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: "rgba(0, 0, 0, 0.75)",
+          },
+          content: {
+            position: "absolute",
+            top: "10rem",
+            left: "16rem",
+            right: "16rem",
+            bottom: "10rem",
+            border: "1px solid #ccc",
+            background: "#fff",
+            overflow: "auto",
+            WebkitOverflowScrolling: "touch",
+            borderRadius: "4px",
+            outline: "none",
+            padding: "20px",
+          },
+        }}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   )
