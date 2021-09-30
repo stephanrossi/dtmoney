6184578a2255d30cadc547743afdf09a372501b2
Stephan Rossi <stephanrossi@gmail.com>
2021-09-30 14:11:15 -0300

    aula 4.4

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ import { Dashboard } from "./components/Dashboard"
 import { GlobalStyle } from "./styles/global"
 
 import Modal from "react-modal"
+import { NewTransactionModal } from "./components/NewTransactionModal"
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root")
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
@@ -23,36 +24,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseTransactionModal}
-        style={{
-          overlay: {
-            position: "fixed",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: "rgba(0, 0, 0, 0.75)",
-          },
-          content: {
-            position: "absolute",
-            top: "10rem",
-            left: "16rem",
-            right: "16rem",
-            bottom: "10rem",
-            border: "1px solid #ccc",
-            background: "#fff",
-            overflow: "auto",
-            WebkitOverflowScrolling: "touch",
-            borderRadius: "4px",
-            outline: "none",
-            padding: "20px",
-          },
-        }}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
       <GlobalStyle />
     </>
   )
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,56 @@
+import Modal from "react-modal"
+import { useState } from "react"
+import { Container, TransactionTypeContainer } from "./styles"
+
+import closeImg from "../../assets/close.svg"
+import incomeImg from "../../assets/income.svg"
+import outcomeImg from "../../assets/outcome.svg"
+
+interface NewTransactionModalProps {
+  isOpen: boolean
+  onRequestClose: () => void
+}
+
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  const [] = useState()
+  const [] = useState()
+  const [] = useState()
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="" />
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input type="text" placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button>
+            <img src={outcomeImg} alt="" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  )
+}
